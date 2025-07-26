1c11342e96092c4679db4126899a844d705e7e04
Bismah Shaikh <113027118+BismahShaikh@users.noreply.github.com>
2025-07-27 01:31:01 +0530

    Delete kyc-service directory

--- kyc-service/src/main/java/com/oracle/kyc/exception/IdentityMismatchException.java
+++
@@ -1,7 +0,0 @@
-package com.oracle.kyc.exception;
-
-public class IdentityMismatchException extends RuntimeException {
-    public IdentityMismatchException(String message) {
-    	super(message);
-    }
-}
--- kyc-service/src/main/java/com/oracle/kyc/exception/KycAlreadyExistsException.java
+++
@@ -1,7 +0,0 @@
-package com.oracle.kyc.exception;
-
-public class KycAlreadyExistsException extends RuntimeException {
-    public KycAlreadyExistsException(String message) {
-    	 super(message);
-    }
-}
